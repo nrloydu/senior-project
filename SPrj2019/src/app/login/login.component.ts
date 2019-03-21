16a4173427aafb427ad5e264f8430bf89722f234
Chris Masterton <christopher.robert.masterton@jci.com>
2019-03-21 14:53:26 -0700

    added vs project workspace

--- SPrj2019/src/app/login/login.component.ts
+++ SPrj2019/src/app/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
         private alertService: AlertService
     ) {
         // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) { 
+        if (this.authenticationService.currentUserValue) {
             this.router.navigate(['/']);
         }
     }
