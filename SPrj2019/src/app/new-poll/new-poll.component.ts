6e690e0b780d6d89291c5b02f9414467bb465865
Mia Kim <miakim@sandiego.edu>
2019-04-30 17:01:39 -0700

    fixing forms

--- SPrj2019/src/app/new-poll/new-poll.component.ts
+++ SPrj2019/src/app/new-poll/new-poll.component.ts
@@ -14,36 +14,41 @@ export class NewPollComponent implements OnInit {
   choice: 'Angular 7';
   // Change this array type to new poll component
   opArr: Array<string>;
+  options: FormArray;
 
-  constructor(
-      private formBuilder: FormBuilder,
-  ) { 
-    this.opArr = [];
-    
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.pollForm = this.formBuilder.group ({
+      pollTitle: '',
       options: this.formBuilder.array(
         [this.initOption()]
       )
-    })
+    });
+  }
+
+  onSubmit(form: FormGoup) {
+    console.log('Valid?', form.valid);
+    console.log('Title', form.value.pollTitle);
   }
 
   initOption() {
     return this.formBuilder.group({
-      choice: ['Edit choice']
+      description: ''
     })
   }
 
-  addOption() {
+  addOption(): void {
     //if (newOption){
-      var opValue = (<HTMLInputElement>document.getElementById("optionBox")).value;
+      /*var opValue = (<HTMLInputElement>document.getElementById("optionBox")).value;
       this.opArr.push(opValue);
-      document.getElementById("target-id").innerText = opValue;
+      document.getElementById("target-id").innerText = opValue;*/
    // }
     // Add the new option to FormArray
     //(this.pollForm.controls['options'] as FormArray).push(this.initOption())
+
+    this.options = this.pollForm.get('options') as FormArray;
+    this.options.push(this.initOption());
   }
 
   removeOption(){
